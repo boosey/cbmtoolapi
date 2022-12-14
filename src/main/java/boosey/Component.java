52aade5e8800e7224d42c0de56bffa3ecc52bd72
BOOSEY BOUDREAUX <john.boudreaux@us.ibm.com>
2022-12-14 13:34:04 -0600

    Fix bug in copying components Added dev time mongo config

--- src/main/java/boosey/Component.java
+++ src/main/java/boosey/Component.java
@@ -14,15 +14,24 @@ public class Component {
     this.id = UUID.randomUUID().toString();
   }
 
+  // Create a copy with a new id
   public Component(Component that) {
     this.id = UUID.randomUUID().toString();
     this.name = that.name;
     this.description = that.description;
+    this.strategic = that.strategic;
+    this.relationship = that.relationship;
+    this.notes = that.notes;
   }
 
+  // Create a copy with the existing id
+  // Used when updating an existing one
   public Component(String componentId, Component that) {
     this.id = componentId;
     this.name = that.name;
     this.description = that.description;
+    this.strategic = that.strategic;
+    this.relationship = that.relationship;
+    this.notes = that.notes;
   }
 }
