79648a496e50d47606ebb132d8f82e631c921e55
BOOSEY BOUDREAUX <john.boudreaux@us.ibm.com>
2022-12-16 12:27:28 -0600

    Added other component dimensions

--- src/main/java/boosey/Component.java
+++ src/main/java/boosey/Component.java
@@ -1,5 +1,6 @@
 package boosey;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Component {
@@ -9,29 +10,48 @@ public class Component {
   public int strategic = 0;
   public int relationship = 0;
   public String notes = "";
+  public boolean isIbmConsulting = false;
+  public boolean isIbmTechnology = false;
+  public boolean isBusiness = false;
+  public boolean isAppDev = false;
+  public boolean isOpsInfra = false;
+  public String businessContact = "";
+  public String appDevContact = "";
+  public String opsInfraContact = "";
+  public ArrayList<String> tags = new ArrayList<>();
 
   public Component() {
     this.id = UUID.randomUUID().toString();
   }
 
+  private static void copyComponentData(Component newThis, Component that) {
+    newThis.name = that.name;
+    newThis.description = that.description;
+    newThis.strategic = that.strategic;
+    newThis.relationship = that.relationship;
+    newThis.notes = that.notes;
+    newThis.isIbmConsulting = that.isIbmConsulting;
+    newThis.isIbmTechnology = that.isIbmTechnology;
+    newThis.isAppDev = that.isAppDev;
+    newThis.isBusiness = that.isBusiness;
+    newThis.isOpsInfra = that.isOpsInfra;
+    newThis.appDevContact = that.appDevContact;
+    newThis.opsInfraContact = that.opsInfraContact;
+    newThis.businessContact = that.businessContact;
+    newThis.tags = new ArrayList<>(that.tags);
+  }
+
   // Create a copy with a new id
   public Component(Component that) {
     this.id = UUID.randomUUID().toString();
-    this.name = that.name;
-    this.description = that.description;
-    this.strategic = that.strategic;
-    this.relationship = that.relationship;
-    this.notes = that.notes;
+    copyComponentData(this, that);
   }
 
   // Create a copy with the existing id
   // Used when updating an existing one
   public Component(String componentId, Component that) {
     this.id = componentId;
-    this.name = that.name;
-    this.description = that.description;
-    this.strategic = that.strategic;
-    this.relationship = that.relationship;
-    this.notes = that.notes;
+    copyComponentData(this, that);
+
   }
 }
